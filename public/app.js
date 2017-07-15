593198b893d313ab54d97278dded7b1e7a3eeee9
y-ota <otausuk@gmail.com>
2017-07-15 22:49:08 +0900

    P67まで

--- public/app.js
+++ public/app.js
@@ -1,11 +1,31 @@
 'use strict'
 var learnjs = {};
 
-learnjs.problemView = function(problemNumber) {
-  var title = 'Problem #' + problemNumber + ' Coming soon!';
-  var view = $('.template .problem-view').clone();
+learnjs.problemView = function(data) {
+  var problemNumber = parseInt(data, 10);
+  var view = $('.templates .problem-view').clone();
+  var problemData = learnjs.problems[problemNumber - 1];
+  var resultFlash = view.find('.result');
+ 
+  function checkAnswer() {
+    var answer = view.find('.answer').val();
+    var test = problemData.code.replace('__', answer) + '; problem();';
+    return eval(test);
+  }
+  function checkAnswerClick() {
+    if (checkAnswer()) {
+     var correctFlash = learnjs.template('correct-flash');
+     correctFlash.find('a').attr('href', '#problem-' + (problemNumber + 1));
+     learnjs.flashElement(resultFlash, 'Correct!');
+    } else {
+     learnjs.flashElement(resultFlash, 'Incorrect!');
+    }
+    return false;
+  }
+
+  view.find('.check-btn').click(checkAnswerClick);
   view.find('.title').text('Problem #' + problemNumber);
-  learnjs.applyObject(learnjs.problems[problemNumber-1], view);
+  learnjs.applyObject(problemData, view);
   return view;
 }
 
@@ -44,3 +64,25 @@ learnjs.applyObject = function(obj, elem) {
   }
 };
 
+learnjs.flashElement= function(elem, content){
+  elem.fadeOut('fast', function() {
+    elem.html(content);
+    elem.fadeIn();
+  })
+};
+
+learnjs.template = function(name) {
+  return $('templates .' + name).clone();
+}
+
+learnjs.buldCorrectFlash = function (problemNum) {
+  var correctFlash = learnjs.template('correct-flash');
+  var link = correctFlash.find('a');
+  if(problemNum < learnjs.problems.length) {
+    link.attr('href','#problem-' + (problemNum + 1));
+  } else {
+    link.attr('href','');
+    linl.text("Your're Finished!");
+  }
+  return correctFlash;
+}
--- public/tests/app_spec.js
+++ public/tests/app_spec.js
@@ -39,4 +39,18 @@ describe('LearnJS', function() {
      expect(view.find('[data-name="code"]').text()).toEqual('function problem() { return __;}');
    });
   });
+  describe('answer selection', function() {
+    it('can check a correct answer by hitting a button', function() {
+      var view = learnjs.problemView('1');
+      view.find('.answer').val('true');
+      view.find('.check-btn').click();
+      expect(view.find('.result').text()).toEqual('Correct!');
+    });
+    it('rejects an incorrect answer', function() {
+      var view = learnjs.problemView('1');
+      view.find('.answer').val('false');
+      view.find('.check-btn').click();
+      expect(view.find('.result').text()).toEqual('Incorrect!');
+    });
+  });
 });
